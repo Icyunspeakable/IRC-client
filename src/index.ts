3fcdcb720200ce0c370fb698db39a91cc77e7b15
matthew1 <gengarodyssey@gmail.com>
2026-05-28 11:36:43 -0500

    pings a peer

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,8 @@ import { createLibp2p } from 'libp2p'
 import { tcp } from '@libp2p/tcp'
 import { noise } from '@chainsafe/libp2p-noise'
 import { yamux } from '@chainsafe/libp2p-yamux'
+import { multiaddr } from '@multiformats/multiaddr'
+import { ping } from '@libp2p/ping'
 
 
 async function main(): Promise<void> {
@@ -19,7 +21,12 @@ async function main(): Promise<void> {
     },
     transports: [tcp()],
     connectionEncrypters: [noise()],
-    streamMuxers: [yamux()]
+    streamMuxers: [yamux()],
+    services: {
+      ping: ping({
+        protocolPrefix: 'ipfs', // default
+      }),
+    },
   })
 
   // start libp2p
@@ -34,10 +41,19 @@ async function main(): Promise<void> {
   const addresses = ['listening on addresses:', ...node.getMultiaddrs().map((addr) => addr.toString())].join('\n') + '\n';
   await appendFile(logPath, addresses, 'utf8');
 
-  // stop libp2p
-  await node.stop()
-  console.log('libp2p has stopped')
-  await appendFile(logPath, 'libp2p has stopped\n', 'utf8');
+  // ping peer if received multiaddr
+  if (process.argv.length >= 3) {
+    const ma = multiaddr(process.argv[2])
+    console.log(`pinging remote peer at ${process.argv[2]}`)
+    await appendFile(logPath, `pinging remote peer at ${process.argv[2]}\n`, 'utf8');
+    const latency = await node.services.ping.ping(ma)
+    console.log(`pinged ${process.argv[2]} in ${latency}ms`)
+    await appendFile(logPath, `pinged ${process.argv[2]} in ${latency}ms\n`, 'utf8');
+  } else {
+    console.log('no remote peer address given, skipping ping')
+    await appendFile(logPath, 'no remote peer address given, skipping ping\n', 'utf8');
+  }
+
 
   const dataDir = path.join(process.cwd(), '.localdata');
   const storage = new JsonlStorage(dataDir);
@@ -57,6 +73,17 @@ async function main(): Promise<void> {
   } else {
     await startTui(app);
   }
+
+  const stop = async () => {
+    // stop libp2p
+    await node.stop()
+    console.log('libp2p has stopped')
+    await appendFile(logPath, 'libp2p has stopped\n', 'utf8');
+    process.exit(0)
+  }
+
+  process.on('SIGTERM', stop)
+  process.on('SIGINT', stop)
 }
 
 void main();
